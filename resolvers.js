204f5607c9bb5fd06ff595460b5b30c6e8b54492
kcabhish <kcabhish@gmail.com>
2024-02-27 07:03:02 -0500

    chore: resolver chaing and documentation

--- resolvers.js
+++ resolvers.js
@@ -2,15 +2,18 @@ import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
     Query: {
-        // Resolver for a single query
-        // job: () => {
-        //     return {
-        //         id: 'someID',
-        //         title: "Cook",
-        //         description: "Cook some awesome food"
-        //     } 
-        // }
-
         jobs: () => getJobs(),
+    },
+
+    // Resolver function has access to the parent property and 
+    // can be passed in as the params
+    Job: {
+        date: (job) => {
+            return toIsoDate(job.createdAt);
+        }
     }
 };
+
+function toIsoDate(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
+}
