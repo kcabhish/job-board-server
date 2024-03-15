4ff43e9b35cdeaaeda124b741863122ff3a6a576
kcabhish <kcabhish@gmail.com>
2024-03-15 04:13:22 -0400

    feat: adding company data to the query and resolver

--- db/companies.js
+++ db/companies.js
@@ -2,6 +2,9 @@ import { connection } from './connection.js';
 
 const getCompanyTable = () => connection.table('company');
 
+export async function getCompanies() {
+  return await getCompanyTable().select();
+}
 export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
 }
--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,24 @@
-import { getJobs } from "./db/jobs.js";
+import { getJob, getJobs } from "./db/jobs.js";
+import { getCompanies, getCompany } from './db/companies.js';
 
 export const resolvers = {
+    /**
+     * In GraphQL, the __root argument typically refers to the root value of the query. It represents the value returned by the parent resolver when resolving the root field of the query.In the context of resolver functions in GraphQL, each resolver receives certain arguments. The first argument is often referred to as the parent value or root value. It contains the result returned by the resolver for the parent field.
+     */
     Query: {
+        company: (__root, { id }) => getCompany(id),
+        companies: () => getCompanies(),
+        job: (__root, { id }) => {
+            console.log('[Query.job] args', id);
+            return getJob(id);
+        },
         jobs: () => getJobs(),
     },
 
     // Resolver function has access to the parent property and 
     // can be passed in as the params
     Job: {
+        company: (job) => getCompany(job.companyId),
         date: (job) => {
             return toIsoDate(job.createdAt);
         }
